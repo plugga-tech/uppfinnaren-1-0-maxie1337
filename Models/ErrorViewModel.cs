1757ec53ef376c5d3e24088133129a729f593b1f
maxie1337 <lonnstrom.max@gmail.com>
2024-12-13 17:26:35 +0100

    Skapat projektet

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace uppfinnaren_1_0_maxie1337.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
