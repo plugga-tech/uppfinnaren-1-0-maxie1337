c72195ed7a42135061722630dda70a9d3e31167e
maxie1337 <lonnstrom.max@gmail.com>
2024-12-22 18:42:29 +0100

    Fixat produkter, bilder och beskrivning

--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -26,24 +26,24 @@ public class AppDbContext : DbContext
                 new Product { ProductId = 4, Name = "Tomte", Description = "En lurig tomte nu i juletider.", ImgUrl = "/Images/CTomte.jpg", Category = "Träprodukter" },
 
                 new Product { ProductId = 5, Name = "Vas", Description = "En vacker keramikvas.", ImgUrl = "/Images/CVase.jpg", Category = "Keramik" },
-                new Product { ProductId = 6, Name = "Mugg", Description = "En handgjord mugg av lera.", ImgUrl = "/Images/CMug.jpg", Category = "Keramik" },
-                new Product { ProductId = 7, Name = "Skål", Description = "En unik handgjord skål.", ImgUrl = "/Images/CBowl.jpg", Category = "Keramik" },
-                new Product { ProductId = 8, Name = "Tallrik", Description = "En elegant tallrik i keramik.", ImgUrl = "/Images/CPlate.jpg", Category = "Keramik" },
+                new Product { ProductId = 6, Name = "Mugg", Description = "En handgjord mugg.", ImgUrl = "/Images/CMug.jpg", Category = "Keramik" },
+                new Product { ProductId = 7, Name = "Skål", Description = "En handgjord skål.", ImgUrl = "/Images/CBowl.jpg", Category = "Keramik" },
+                new Product { ProductId = 8, Name = "Tallrik", Description = "En hemmagjord tallrik.", ImgUrl = "/Images/CPlate.jpg", Category = "Keramik" },
 
-                new Product { ProductId = 9, Name = "Halsband", Description = "Ett elegant halsband i silver.", ImgUrl = "/Images/CNecklace.jpg", Category = "Smycken" },
-                new Product { ProductId = 10, Name = "Armband", Description = "Ett armband med pärlor.", ImgUrl = "/Images/CBracelet.jpg", Category = "Smycken" },
-                new Product { ProductId = 11, Name = "Ring", Description = "En vacker ring med en diamant.", ImgUrl = "/Images/CRing.jpg", Category = "Smycken" },
-                new Product { ProductId = 12, Name = "Hänge", Description = "Ett stilrent hänge i guld.", ImgUrl = "/Images/CHanging.jpg", Category = "Smycken" },
+                new Product { ProductId = 9, Name = "Halsband", Description = "Ett elegant halsband för alla gangstahs.", ImgUrl = "/Images/CNecklace.jpg", Category = "Smycken" },
+                new Product { ProductId = 10, Name = "Armband", Description = "Armband gjorda på fritids.", ImgUrl = "/Images/CBracelet.jpg", Category = "Smycken" },
+                new Product { ProductId = 11, Name = "Ring", Description = "Ringen av kraft.", ImgUrl = "/Images/CRing.jpg", Category = "Smycken" },
+                new Product { ProductId = 12, Name = "Örhänge", Description = "Luriga örhängen.", ImgUrl = "/Images/CHanging.jpg", Category = "Smycken" },
 
                 new Product { ProductId = 13, Name = "Tavla", Description = "En vacker tavla med landskap.", ImgUrl = "/Images/CPainting.jpg", Category = "Konst" },
                 new Product { ProductId = 14, Name = "Skulptur", Description = "En modern skulptur i sten.", ImgUrl = "/Images/CStatue.jpg", Category = "Konst" },
-                new Product { ProductId = 15, Name = "Fotografi", Description = "En vacker svartvit fotografi.", ImgUrl = "/Images/CPhoto.jpg", Category = "Konst" },
-                new Product { ProductId = 16, Name = "Målning", Description = "En abstrakt målning med starka färger.", ImgUrl = "/Images/CAbstractPainting.jpg", Category = "Konst" },
+                new Product { ProductId = 15, Name = "Foto", Description = "Ett svartvitt foto.", ImgUrl = "/Images/CPhoto.jpg", Category = "Konst" },
+                new Product { ProductId = 16, Name = "Målning", Description = "En målning av tomten.", ImgUrl = "/Images/CPaint.jpg", Category = "Konst" },
 
-                new Product { ProductId = 17, Name = "Lampa", Description = "En retro bordslampa.", ImgUrl = "/Images/CLamp.jpg", Category = "Inredning" },
-                new Product { ProductId = 18, Name = "Matta", Description = "En handknuten matta.", ImgUrl = "/Images/CRug.jpg", Category = "Inredning" },
-                new Product { ProductId = 19, Name = "Spegel", Description = "En elegant spegel med träram.", ImgUrl = "/Images/CMirror.jpg", Category = "Inredning" },
-                new Product { ProductId = 20, Name = "Kudde", Description = "En mjuk dekorativ kudde.", ImgUrl = "/Images/CPillow.jpg", Category = "Inredning" }
+                new Product { ProductId = 17, Name = "Lampa", Description = "En måne som går att ha inomhus.", ImgUrl = "/Images/CLamp.jpg", Category = "Inredning" },
+                new Product { ProductId = 18, Name = "Matta", Description = "En skum matta.", ImgUrl = "/Images/CRug.jpg", Category = "Inredning" },
+                new Product { ProductId = 19, Name = "Spegel", Description = "En elegant spegel med alkoholtips i.", ImgUrl = "/Images/CMirror.jpg", Category = "Inredning" },
+                new Product { ProductId = 20, Name = "Kudde", Description = "En snygg kudde.", ImgUrl = "/Images/CPillow.jpg", Category = "Inredning" }
                 
         );
     }
--- obj/Debug/net9.0/uppfinnaren-1-0-maxie1337.AssemblyInfo.cs
+++ obj/Debug/net9.0/uppfinnaren-1-0-maxie1337.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("uppfinnaren-1-0-maxie1337")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e80e1341bc89c5d853014d1c1672ad5286fb898")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6094cce3a9e77e239bac55cf4c9722918d541a6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("uppfinnaren-1-0-maxie1337")]
 [assembly: System.Reflection.AssemblyTitleAttribute("uppfinnaren-1-0-maxie1337")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
